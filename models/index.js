969d5ab8581d3a2cc126aba06e8c86b1b05521ac
Lola Applegate <laurenemilya@gmail.com>
2022-05-31 13:12:30 -0700

    created database schema, started on table connections

--- models/index.js
+++ models/index.js
@@ -2,3 +2,16 @@
 // const Course = require('./Course');
 
 // module.exports = { Student, Course };
+
+
+const User = require('./User');
+const Category = require('./Category');
+const Chicken = require('./Chicken');
+const Accessory = require('./Accessory');
+
+User.hasOne(Chicken);
+Chicken.belongsTo(User);
+
+
+
+module.exports = { User, Category, Chicken, Accessory };
