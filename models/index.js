7d8453847f11261534963fe256cd929c5d8bf5da
Lola Applegate <laurenemilya@gmail.com>
2022-05-31 11:52:58 -0700

    fixed connection.js, added user model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const Student = require('./Student');
-const Course = require('./Course');
+// const Student = require('./Student');
+// const Course = require('./Course');
 
-module.exports = { Student, Course };
+// module.exports = { Student, Course };
