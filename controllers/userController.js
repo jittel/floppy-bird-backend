e9e7c2346d78251fa0319d4be31e7ee4bad9e836
jittel <nathan.c.ginn@gmail.com>
2022-06-02 13:22:39 -0700

    fixed sign up route, now adds chicken when you add a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const { User, Chicken } = require('../models')
+const jwt = require("jsonwebtoken");
 
 module.exports = {
     // Get all users
@@ -18,17 +19,51 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
-    // create a new user
-    createUser(req, res) {
-        User.create(req.body)
-            .then((user) => {
-                if (req.body.chickenName) {
-                    return Chicken.create(req.body.chickenName)
-                }
-                res.status(200).json(user)
+    async createUser(req, res) {
+        try {
+            const newUser = await User.create(req.body)
+            const newChicken = await Chicken.create({
+                chicken_name: req.body.chicken_name,
+                user_id: newUser.id
             })
-            .then((chickenId) => res.status(200).json(chickenId))
-            .catch((err) => res.status(500).send(err));
+            const token = jwt.sign({
+                userId: newUser.id
+            }, process.env.JWT_SECRET, {
+                expiresIn: "6h"
+            })
+            return res.json({
+                user: newUser,
+                chicken: newChicken,
+                token: token
+            })
+        } catch (err) {
+            console.log(err)
+            res.status(400).json(err)
+        }
+    },
+    //log in to user
+    login(req, res) {
+        User.findOne({
+            where: {
+                username: req.body.username
+            }
+        }).then(foundUser => {
+            if (!foundUser) {
+                return res.status(401).json({ msg: "invalid login credentials" })
+            }
+            if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+                const token = jwt.sign({
+                    userId: foundUser.id
+                }, process.env.JWT_SECRET, {
+                    expiresIn: "6h"
+                })
+                return res.json({
+                    user: foundUser,
+                    token: token
+                })
+            }
+            return res.status(401).json({ msg: "invalid login credentials" })
+        })
     },
     // Delete a user and associated apps
     deleteUser(req, res) {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,6 +3,7 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  login
 } = require('../../controllers/userController.js');
 
 // /api/users
@@ -10,5 +11,8 @@ router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser);
+
+// /api/login
+router.route('/login').post(login)
 
 module.exports = router;
