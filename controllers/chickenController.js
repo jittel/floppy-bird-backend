58e253f31163c3a6e4719940c2b50f30bd59f099
jittel <nathan.c.ginn@gmail.com>
2022-06-02 14:17:05 -0700

    fixed some chicken routes, added get a single chicken to api, chicken now has associated userId

--- controllers/chickenController.js
+++ controllers/chickenController.js
@@ -1,9 +1,21 @@
-const { Chicken } = require('../models')
+const { Chicken, User } = require('../models')
 
 module.exports = {
     getChickens(req,res) {
         Chicken.findAll()
             .then((chickens) => res.json(chickens))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Get a single user
+    getSingleChicken(req, res) {
+        Chicken.findByPk(req.params.id, {
+            include:[User]
+        })
+            .then((chicken) =>
+                !chicken
+                    ? res.status(404).json({ message: 'No chicken with that ID' })
+                    : res.json(chicken)
+            )
             .catch((err) => res.status(500).json(err));
     }
 }
