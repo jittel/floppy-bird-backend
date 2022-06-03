a86213c96065ddcad0ad02ac8165a3fed7baf591
jittel <nathan.c.ginn@gmail.com>
2022-06-03 11:49:16 -0700

    accessory routes finished p sure

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes')
 const chickenRoutes = require('./chickenRoutes')
+const accessoryRoutes = require('./accessoryRoutes')
 
 router.use('/users', userRoutes);
 router.use('/chickens', chickenRoutes);
+router.use('/accessories', accessoryRoutes);
 
 module.exports = router;
