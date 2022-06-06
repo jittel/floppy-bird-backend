0568037a1020d2ca800e467db3649a6da9cfd7cb
Lola Applegate <laurenemilya@gmail.com>
2022-06-06 15:15:40 -0700

    seeded accessory data

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,11 +2,16 @@ const sequelize = require('../config/connection');
 const { Accessory } = require('../models');
 
 const hatSeedData = require('./hatSeedData.json');
+const shoeSeedData = require('./shoeSeedData.json');
+const armSeedData = require('./armSeedData.json');
 
 const seedMe = async () => {
     await sequelize.sync({ force: true });
 
     const hats = await Accessory.bulkCreate(hatSeedData);
+    const shoes = await Accessory.bulkCreate(shoeSeedData);
+    const arms = await Accessory.bulkCreate(armSeedData);
+
     process.exit(0);
 };
 
