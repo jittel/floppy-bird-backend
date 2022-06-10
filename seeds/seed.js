39587cbf3199f11fcf8756ece7139d4c6208714a
Lola Applegate <laurenemilya@gmail.com>
2022-06-10 12:06:07 -0700

    user & chicken seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,8 +4,9 @@ const { Accessory, Category, User, Chicken } = require('../models');
 const hatSeedData = require('./hatSeedData.json');
 const shoeSeedData = require('./shoeSeedData.json');
 const armSeedData = require('./armSeedData.json');
-
 const catSeedData = require('./categorySeedData.json');
+const userSeedData = require('./userSeedData.json');
+const chiSeedData = require('./chickenSeedData.json');
 
 const seedMe = async () => {
     await sequelize.sync({ force: true });
@@ -15,14 +16,17 @@ const seedMe = async () => {
     const hats = await Accessory.bulkCreate(hatSeedData);
     const shoes = await Accessory.bulkCreate(shoeSeedData);
     const arms = await Accessory.bulkCreate(armSeedData);
-    const newUser = await User.create({
-        username: "jeff",
-        password: "password",
-    })
-    const newChicken = await Chicken.create({
-        chicken_name:"jeff2"
-    })
-    await newUser.addAccessory(2)
+    const users = await User.bulkCreate(userSeedData);
+    const chickies = await Chicken.bulkCreate(chiSeedData);
+
+    // const newUser = await User.create({
+    //     username: "jeff",
+    //     password: "password",
+    // })
+    // const newChicken = await Chicken.create({
+    //     chicken_name:"jeff2"
+    // })
+    // await users.addAccessory(2)
     
     process.exit(0);
 };
